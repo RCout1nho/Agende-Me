6e8ff2feb3c973941c1b196c4d149cc36ff1664d
Vandrekad <orlandovandres@gmail.com>
2020-04-14 15:21:59 -0300

    booking commit

--- src/pages/Booking/index.js
+++ src/pages/Booking/index.js
@@ -1,12 +1,101 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Dropdown } from 'react-native-material-dropdown';
 
-import { Container, StatusBar } from './styles';
+import { MaterialIcons } from '@expo/vector-icons';
 
-export default function Booking() {
+import {
+  Container, StatusBar, ContainerTitle, NameMarket, FavButton, TextTitle, ImgCarousel, ContainerMarket, ContainerList, ContainerAdress, TextAdress, Adress, ContainerDropdown, ScheduleButton, Text
+} from './styles';
+
+export default function Booking(handleLike) {
+  const [liked, setLiked] = useState(false);
+
+  function handleLike() {
+    setLiked(!liked);
+  };
+
+  let data = [{
+    value: '1',
+  }, {
+    value: '2',
+  }, {
+    value: '3',
+  }, {
+    value: '4',
+  }, {
+    value: '5',
+  }, {
+    value: '6',
+  },];
+
   return (
     <Container>
       <StatusBar />
+      <ImgCarousel>
+
+      </ImgCarousel>
+      <ContainerMarket>
+        <ContainerTitle>
+          <NameMarket>
+            <TextTitle>Market</TextTitle>
+          </NameMarket>
+          <FavButton activeOpacity={0.5} onPress={() => { handleLike() }}  ><MaterialIcons
+            name={liked ? "favorite" : "favorite-border"}
+            size={30}
+            color={liked ? "#FF0000" : "#D9D0E3"}
+          /></FavButton>
+        </ContainerTitle>
+        <ContainerAdress>
+          <MaterialIcons name="home" size={20} />
+          <Adress>
+            <TextAdress>
+              Alexandra Smith
+            </TextAdress>
+            <TextAdress>
+              Cesu 31 k-2 5.st, SIA Chili
+            </TextAdress>
+          </Adress>
+        </ContainerAdress>
+        <ContainerList>
+          <ContainerDropdown>
+            <Dropdown
+              label='Escolha o dia'
+              data={data}
+              itemPadding={8}
+              fontSize={21}
+              baseColor={"#868686"}
+              containerStyle={({
+                borderRadius: 13,
+                height: 56,
+                marginBottom: 10,
+                backgroundColor: '#FFFFFF',
+                justifyContent: "center",
+                paddingLeft: 10
+              })}
+            />
+            <Dropdown
+              label='Escolha o horário'
+              data={data}
+              itemPadding={8}
+              fontSize={21}
+              baseColor={"#868686"}
+              containerStyle={({
+                borderRadius: 13,
+                height: 56,
+                marginBottom: 10,
+                backgroundColor: '#FFFFFF',
+                justifyContent: "center",
+                paddingLeft: 10
+              })}
+            />
+            <ScheduleButton>
+              <Text>
+                Schedule
+              </Text>
+            </ScheduleButton>
+          </ContainerDropdown>
+        </ContainerList>
+      </ContainerMarket>
     </Container>
   )
 }
--- src/pages/Booking/styles.js
+++ src/pages/Booking/styles.js
@@ -8,4 +8,92 @@ export const StatusBar = styled.View`
 
 export const Container = styled.View`
   flex: 1;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const ImgCarousel = styled.Image`
+  height:300px;
+`;
+
+export const ContainerMarket = styled.View`
+  background: #F6F5F5;
+  height: 532px;
+  border-top-right-radius: 40px;
+  border-top-left-radius: 40px;
+  border-width: 1px;
+  elevation: 20;
+`;
+
+export const ContainerTitle = styled.View`
+  flex-direction: row;
+  height: 41px;
+  width: 374px;
+  margin-top: 37px;
+  margin-left: 20px;
+`;
+
+export const ContainerAdress = styled.View`
+  border-bottom-width: 1px;
+  margin-top:16px;
+  margin-left: 44.5px;
+  margin-right: 44.5px;
+  padding-bottom: 10px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ContainerList = styled.View`
+  margin-top:16px;
+  height:199px;
+  width:325px;
+  margin-left: 44.5px;
+`;
+
+export const ContainerDropdown = styled.View`
+`;
+
+export const FavButton = styled.TouchableOpacity`
+  width: 80px;
+  height: 50px;
+  background: #fff;
+  border-color: #D9D0E3;
+  border-width: 1px;
+  border-radius: 8px;
+  align-items: center;
+  justify-content: center;
+  elevation: 10;
+`;
+
+export const ScheduleButton = styled.TouchableOpacity`
+  width: 165px;
+  height: 44px;
+  background-color: #3BC365;
+  justify-content: center;
+  border-radius: 13px;
+  margin: auto;
+`;
+
+export const NameMarket = styled.View`
+  height: 41px;
+`;
+
+export const TextTitle = styled.Text`
+  font-size:30px;
+  padding-right: 20px;
+  font-weight:bold;
+`;
+
+export const TextAdress = styled.Text`
+  font-size: 17px;
+  text-align: left;
+`;
+
+export const Text = styled.Text`
+  color: #fff;
+  text-align: center;
+`;
+
+export const Adress = styled.View`
+  margin-left: 3px;
 `;
